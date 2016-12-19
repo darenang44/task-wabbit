246b013d57869f26cd23f1ef877dd2762188ab02
darenang44 <darenang44@gmail.com>
2016-12-19 14:39:58 -0800

    Add edit and destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,12 +24,19 @@ class TasksController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
+
   def destroy
+    @task.destroy
+    redirect_to root_path
   end
 
 
