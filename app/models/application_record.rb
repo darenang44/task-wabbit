00031347fbaca4ad6a1dcd1c433baa0fc8a067c0
darenang44 <darenang44@gmail.com>
2016-12-19 14:18:45 -0800

    Add simple form, bootstrap sass, actions, and override names route parameters

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+  def to_param
+    title
+  end
   self.abstract_class = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks, param: :title
+  root 'tasks#index'
 end
+
+# Prefix Verb   URI Pattern                  Controller#Action
+#     tasks GET    /tasks(.:format)             tasks#index
+#           POST   /tasks(.:format)             tasks#create
+#  new_task GET    /tasks/new(.:format)         tasks#new
+# edit_task GET    /tasks/:title/edit(.:format) tasks#edit
+#      task GET    /tasks/:title(.:format)      tasks#show
+#           PATCH  /tasks/:title(.:format)      tasks#update
+#           PUT    /tasks/:title(.:format)      tasks#update
+#           DELETE /tasks/:title(.:format)      tasks#destroy
